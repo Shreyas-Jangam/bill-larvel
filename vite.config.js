9d65d21f059ed16c8ecd1e60d3a365a34bf78ff1
Shreyas Jangam <shreyasjangamofficial@gmail.com>
2025-03-18 12:51:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,4 +11,8 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build', // Ensure Vercel recognizes this as the output directory
+        emptyOutDir: true,
+    },
 });
